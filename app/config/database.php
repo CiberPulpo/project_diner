264031ed4ccdc0d96157c2861318fcd31bae40b6
CiberPulpo <ipal_1993@hotmail.com>
2014-04-24 04:37:56 -0500

    MIGRACION 1er sprint

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'bd_acunica',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'java_777',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
---
+++ app/database/migrations/2014_04_24_023745_create_comedor.php
@@ -0,0 +1,145 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComedor extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('permisoRecojo', function($tabla) {
+			$tabla->increments('id');
+			$tabla->string('descripcion',50);
+		});
+
+		// Schema::create('cargo', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		Schema::create('usuario', function($tabla) {
+			$tabla->increments('id');
+			$tabla->string('nombre',45);
+			$tabla->string('descripcion',100);
+			$tabla->boolean('estado');
+		});
+
+		Schema::create('facultad', function($tabla) {
+			$tabla->increments('id');
+			$tabla->string('nombre',50);
+			$tabla->boolean('estado');
+		});
+
+		Schema::create('escuela', function($tabla) {
+			$tabla->increments('id');
+			$tabla->string('nombre',50);
+			$tabla->boolean('estado');
+			$tabla->integer('facultad_id')->unsigned();
+			$tabla->foreign('facultad_id')->references('id')->on('facultad');
+		});
+
+		Schema::create('estudiante', function($tabla) {
+			$tabla->increments('id');
+			$tabla->string('dni',8);
+			$tabla->string('numCarnetUniversitario',10);
+			$tabla->string('nombre');
+			$tabla->string('apellidoPaterno',45);
+			$tabla->string('apellidoMaterno',45);
+			$tabla->date('anioIngreso');
+			$tabla->string('direccion',70);
+			$tabla->string('sexo',1);
+			$tabla->date('fechaNacimiento');
+			$tabla->string('imagen');
+			$tabla->string('sis',10);
+			$tabla->string('numBono',4);
+			$tabla->boolean('estadomatricula');
+			$tabla->string('numExpediente',6);
+			$tabla->boolean('estado');
+			$tabla->integer('permisoRecojo_id')->unsigned();
+			$tabla->foreign('permisoRecojo_id')->references('id')->on('permisoRecojo');
+			$tabla->integer('usuario_id')->unsigned();
+			$tabla->foreign('usuario_id')->references('id')->on('usuario');
+			$tabla->integer('escuela_id')->unsigned();
+			$tabla->foreign('escuela_id')->references('id')->on('escuela');
+		});
+
+
+
+		// Schema::create('sancion', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('personal', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('estudianteTemporal', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('registroApoyo', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('asistencia', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('ventaBono', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('detalle_ventaBono', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+
+
+		// Schema::create('matricula', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('cargo_historia', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('privilegios', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+
+		// Schema::create('asignacion', function($tabla) {
+		// 	$tabla->increments('id');
+		// 	$tabla->timestamps();
+		// });
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('permisoRecojo');
+		Schema::drop('usuario');
+		Schema::drop('facultad');
+		Schema::drop('escuela');
+		Schema::drop('estudiante');
+	}
+
+}
